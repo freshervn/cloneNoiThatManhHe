8b9032e224a7732f4674f629fe811f54374fb7f0
freshervn <thefagogroup>
2021-06-27 15:01:59 +0700

    fix scroll

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import '../style/footer.scss';
 import btn_scroll from '../images/back-to-top.png';
 class Footer extends Component {
@@ -7,7 +6,6 @@ class Footer extends Component {
     render() { 
         return ( 
             <React.StrictMode>
-                <span id="topPages"></span>  
                 <a href='#topPages' id='btn-scroll'>
                     <img src={btn_scroll} alt="btn-scroll" />
                 </a>
