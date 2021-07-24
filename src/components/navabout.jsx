3354c407390e6407a2bb4a1ac4d40beb9ac493ba
freshervn <thefagogroup>
2021-07-24 15:35:51 +0700

    up navbar mobie

--- src/components/navabout.jsx
+++ src/components/navabout.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import "../style/navmobie.scss";
+import "../style/navabout.scss";
 function NavAbout(){
     var screenwidth = window.innerWidth
     if (screenwidth < 992) {       
         return(
-            <nav className='d-flex justify-content-between border-bottom'>
+            <nav className='row justify-content-between border-bottom about-nav px-3'>
                 <Link to='/' className='btn bg-transparent'>
                     <img src={require('../images/icons/preview.png').default } alt="" className='h-1'/>
                 </Link>
@@ -14,12 +14,9 @@ function NavAbout(){
                         Bùi Đạt
                     </b>
                 </p>
-                <label className="">
-                    <div className="">
-                        <i data-visualcompletion="css-img" class="facebookSearch" >
-                        </i>
-                    </div>
-                    <input type="search" dir="ltr" aria-autocomplete="list" aria-expanded="false" aria-label="Tìm kiếm trên Facebook" role="combobox" placeholder="Tìm kiếm trên Facebook" autocomplete="off" name="global_typeahead" spellcheck="false" aria-invalid="false" value="" class="search"/>
+                <label className="search btn mb-0">
+                    <i data-visualcompletion="css-img" class="facebookSearch" ></i>
+                    <input type="search" dir="ltr" aria-autocomplete="list" aria-expanded="false" aria-label="Tìm kiếm trên Facebook" role="combobox" placeholder="Tìm kiếm trên Facebook" autocomplete="off" name="global_typeahead" spellcheck="false" aria-invalid="false" value=""/>
                 </label>
             </nav>
         );
