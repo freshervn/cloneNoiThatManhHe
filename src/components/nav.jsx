c6cd63fa2f2950b0c19627df9885f550bc41b949
freshervn <thefagogroup>
2021-07-24 18:45:58 +0700

    up date navbar

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Navbarmobie from './Navbarmobie';
+import Navbardesktop from './Navbardesktop';
 function Navbar() { 
-    var windowsize= window.width;
-    if (windowsize<768) {        
+    var windowsize = window.innerWidth;
+    console.log(windowsize);
+    if (windowsize < 992) {        
         return (          
             <Navbarmobie/>
         );        
     }
-    return (          
-        <Navbarmobie/>
-    );
+    else{
+        return (          
+            <Navbardesktop/>
+        );
+    }
 }
 export default Navbar;
